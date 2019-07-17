6c64dcf2cd98731489c2900360cc0cc6b02c208d
Benjamin <benjamin@MBP-de-Benjamin.lan>
2019-07-17 17:54:47 +0200

    MAJ

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
